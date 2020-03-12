db81df5fe4809f290210ca62248d51892185e981
Pham Hong Ky <hongkyhvnh@gmail.com>
2020-03-12 23:15:43 +0700

    Project router 50%

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -22,7 +22,7 @@ class App extends Component {
         <Router>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/home">Home</Link>
             </li>
             <li>
               <Link to="/form">Form</Link>
@@ -33,7 +33,7 @@ class App extends Component {
           </ul>
           <hr style={{ width: "200px" }} />
           <Switch>
-            <Route path="/" exact children={<Home />} />
+            <Route path="/home" exact children={<Home />} />
             <Route path="/form" children={<Form />} />
             <Route path="/about" children={<About />} />
           </Switch>
--- react-router/src/components/home.jsx
+++ react-router/src/components/home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { loadData } from "../actions/useraction";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class Home extends Component {
   componentDidMount = () => {
@@ -21,25 +22,51 @@ class Home extends Component {
     console.log("userInfo", userInfo);
 
     return (
-      <div className="userInfo">
-        {userInfo.isLoaded ? (
-          <>
-            <h2>
-              {`${userInfo.item[0].name.title} 
+      <Router>
+        <div className="userInfo">
+          {userInfo.isLoaded ? (
+            <>
+              <h2>
+                {`${userInfo.item[0].name.title} 
               ${userInfo.item[0].name.first} 
                 ${userInfo.item[0].name.last}`}
-            </h2>
-            <div className="userlogo">
-              <img src={userInfo.item[0].picture.large} alt="USER IMAGE" />
-            </div>
-            <table>
-              <tr></tr>
-            </table>
-          </>
-        ) : (
-          <h2>Loading</h2>
-        )}
-      </div>
+              </h2>
+              <div className="userlogo">
+                <img src={userInfo.item[0].picture.large} alt="USER IMAGE" />
+              </div>
+
+              <div className="user-info">
+                <div className="info__header">
+                  <div className="info__row--item">
+                    <Link to="/home/info">General Info</Link>
+                  </div>
+                  <div className="info__row--item">
+                    <Link to="/home/adress">Adress</Link>
+                  </div>
+                  <div className="info__row--item">
+                    <Link to="/home/account">Account</Link>
+                  </div>
+                </div>
+                <div className="info__body">
+                  <Switch>
+                    <Route exact path="/home/info">
+                      <p>This is General Infomation</p>
+                    </Route>
+                    <Route path="/home/adress">
+                      <p>This is Adress</p>
+                    </Route>
+                    <Route path="/home/account">
+                      <p>This is Account</p>
+                    </Route>
+                  </Switch>
+                </div>
+              </div>
+            </>
+          ) : (
+            <h2>Loading</h2>
+          )}
+        </div>
+      </Router>
     );
   }
 }
