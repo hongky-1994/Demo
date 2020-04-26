d9a37bf55e6cbf85d5f9cd0f201e3cee4dd103b4
Pham Hong Ky <hongkyhvnh@gmail.com>
2020-04-27 01:54:46 +0700

    Login page complete, working on data

--- drivingTest/js/convertExcel.js
+++ drivingTest/js/convertExcel.js
@@ -1,76 +1,78 @@
-const input = document.querySelector('#excel')
-const btn = document.querySelector('#uploadExcel')
-const result = document.querySelector('#results')
+const convert = () => {
+  const input = document.querySelector('#excel')
+  const btn = document.querySelector('#uploadExcel')
+  const result = document.querySelector('#results')
 
-var excelFile;
+  var excelFile;
 
-input.addEventListener('change', (e) => {
-  excelFile = e.target.files[0]
-})
+  input.addEventListener('change', (e) => {
+    excelFile = e.target.files[0]
+  })
 
 
-btn.addEventListener('click', () => {
-  if(excelFile) {
-    console.log('Starting converting file ...')
-    var fileReader = new FileReader();
-    
-    console.log('fileReader', fileReader)
-    fileReader.onload = function(e) {
+  btn.addEventListener('click', () => {
+    if(excelFile) {
+      console.log('Starting converting file ...')
+      var fileReader = new FileReader();
       
-      // Get Binary data 
-      var data = e.target.result;
+      console.log('fileReader', fileReader)
+      fileReader.onload = function(e) {
+        
+        // Get Binary data 
+        var data = e.target.result;
 
-      // Get Workbook data from binary
-      var workbook = XLSX.read(data, {type: "binary"})
-      console.log('Workbook convert', workbook)
+        // Get Workbook data from binary
+        var workbook = XLSX.read(data, {type: "binary"})
+        console.log('Workbook convert', workbook)
 
-      // Get data from each Sheet in workbook
-      workbook.SheetNames.forEach(sheet => {
-        let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
-        console.log('RowObject', rowObject)
+        // Get data from each Sheet in workbook
+        workbook.SheetNames.forEach(sheet => {
+          let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
+          console.log('RowObject', rowObject)
 
-        const array= JSON.stringify(rowObject)
-        showInTable(rowObject, result)
-      })
+          const array= JSON.stringify(rowObject)
+          showInTable(rowObject, result)
+        })
 
+      }
     }
-  }
-  fileReader.readAsBinaryString(excelFile)
-})
-
-showInTable = (array, object) => {
-  let tableHeader = ''
-
-  console.log( array[0])
-
-  Object.keys(array[0]).forEach(item => {
-    tableHeader +=`<th class="table__head">${item}</th>`
+    fileReader.readAsBinaryString(excelFile)
   })
-  
-  
-  
-  let tableData = ''
-  
 
-  array.forEach(item => {
-    let row = "<tr class='table__row'>"
-    Object.values(item).forEach(tdData => {
-      row += `<td class="table__data">${tdData}</td>`
+  showInTable = (array, object) => {
+    let tableHeader = ''
+
+    console.log( array[0])
+
+    Object.keys(array[0]).forEach(item => {
+      tableHeader +=`<th class="table__head">${item}</th>`
     })
-    row +="</tr>"
-    tableData += row
-  })
-  
-  
-  object.innerHTML = `
-  <table class='table'>
-      <thead>
-        <tr>${tableHeader}</tr>
-      </thead>
-      <tbody>
-        ${tableData}
-      </tbody>
-    </table>
-  `
-  
+    
+    
+    
+    let tableData = ''
+    
+
+    array.forEach(item => {
+      let row = "<tr class='table__row'>"
+      Object.values(item).forEach(tdData => {
+        row += `<td class="table__data">${tdData}</td>`
+      })
+      row +="</tr>"
+      tableData += row
+    })
+    
+    
+    object.innerHTML = `
+    <table class='table'>
+        <thead>
+          <tr>${tableHeader}</tr>
+        </thead>
+        <tbody>
+          ${tableData}
+        </tbody>
+      </table>
+    `
+    
+  }
 }
